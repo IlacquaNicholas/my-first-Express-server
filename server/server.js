62d90a25716920ae1174eac22f5edccf3dfa6321
Nicholas Ilacqua <Niservices88@gmail.com>
2021-11-10 10:15:40 -0600

    learned how to send stuff to the browser

--- server/server.js
+++ server/server.js
@@ -13,6 +13,32 @@ const app = express();
  //aka "all the stuff we've been happily doing already"
  app.use(express.static('./server/public'));
 
+ //GET requests with data. 
+ const message = 'Hello from the server';
+
+ //Route function, Endpoint
+ //req (request), object
+ //res (request), object
+app.get('/message', (req, res) => {
+
+console.log('in the get route for /message');
+//we need to respond
+res.send(message);
+})
+//New array
+const dogs = ['marmaduke', 'Snoopy', 'Scooby Doo'];
+
+app.get('/dogs', (req, res) => {
+   console.log('in get for /dogs');
+   
+   res.send(dogs)
+})
+
+app.get('/number',  (req, res)=>{
+   res.send({ number: 7 });
+
+})
+
 //Starts server, and listens for requests:
  app.listen(5000, function() {
     console.log('Your first express server is running!');
